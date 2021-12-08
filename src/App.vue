1b88f184799aa8eb024a5a59d2a37327ae4cd067
yevgeller <yevgeller@gmail.com>
2021-12-07 23:00:23 -0600

    Getting styling done for PageHome.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <page-home />
+  <div class="container"><page-home /></div>
 </template>
 
 <script>
