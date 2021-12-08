8194cfc81380e41ef8bef8c607aa50e0b689ecc4
yevgeller <yevgeller@gmail.com>
2021-12-07 21:57:36 -0600

    Before installing composition-api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <page-home />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import PageHome from '@/components/PageHome'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PageHome
   }
 }
 </script>
