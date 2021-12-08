099618ce05f94541f9407c6cdb15d6a6bd6d1257
yevgeller <yevgeller@gmail.com>
2021-12-07 22:28:40 -0600

    After chapter 10 -- got PageHome with threads and posts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   <page-home />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import PageHome from '@/components/PageHome'
+import PageHome from "@/components/PageHome";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
-    PageHome
-  }
-}
+    PageHome,
+  },
+};
 </script>
 
 <style>
--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>Hello from PageHome</div>
+  <div>Hello from PageHome</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>
+        {{ userById(postById(postId).userId).name }}
+      </p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+  </div>
 </template>
 <script>
-import { defineComponent } from '@vue/composition-api'
-
-export default defineComponent({
-    setup() {
-        
+import sourceData from "@/data.json";
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+  methods: {
+    postById(postId) {
+      return this.posts.find((p) => p.id === postId);
     },
-})
+    userById(userId) {
+      return this.users.find((u) => u.id === userId);
+    },
+  },
+};
 </script>
 <style scoped>
-
 </style>
