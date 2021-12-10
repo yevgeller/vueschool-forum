a75d43031ab0adea7d8c6ed32325f3b604e10ae3
yevgeller <yevgeller@gmail.com>
2021-12-09 23:04:38 -0600

    Created a router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <div class="container"><page-home /></div>
+  <div class="container"><router-view /></div>
 </template>
 
 <script>
-import PageHome from "@/components/PageHome";
 export default {
   name: "App",
-  components: {
-    PageHome,
-  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import PageHome from "@/components/PageHome";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const routes = [{ path: "/", name: "Home", component: PageHome }];
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+});
+
+const forumApp = createApp(App);
+forumApp.use(router);
+forumApp.mount("#app");
