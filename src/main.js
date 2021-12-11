fcb15820cc9c9448ec2a99c7d3758e8ecad208ef
yevgeller <yevgeller@gmail.com>
2021-12-10 22:26:40 -0600

    Broken PageThreadShow.vue

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import PageHome from "@/components/PageHome";
+import PageThreadShow from "@/components/PageThreadShow";
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [{ path: "/", name: "Home", component: PageHome }];
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: PageHome,
+  },
+  {
+    path: "/thread/:id",
+    name: "ThreadShow",
+    component: PageThreadShow,
+    props: true,
+  },
+];
 
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHistory(),
-  routes, // short for `routes: routes`
+  routes,
 });
-
 const forumApp = createApp(App);
 forumApp.use(router);
 forumApp.mount("#app");
