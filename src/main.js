2f74768a313597edcbb99c4c6e75d106ec3cfb13
yevgeller <yevgeller@gmail.com>
2021-12-10 22:58:25 -0600

    Cleaned up the router

--- src/main.js
+++ src/main.js
@@ -1,27 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import PageHome from "@/components/PageHome";
-import PageThreadShow from "@/components/PageThreadShow";
-import { createRouter, createWebHistory } from "vue-router";
+import router from "@/router";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: PageHome,
-  },
-  {
-    path: "/thread/:id",
-    name: "ThreadShow",
-    component: PageThreadShow,
-    props: true,
-  },
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
 const forumApp = createApp(App);
 forumApp.use(router);
 forumApp.mount("#app");
