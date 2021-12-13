d5d000af909c6e3b45351ecc8d99a4f59486626c
yevgeller <yevgeller@gmail.com>
2021-12-12 22:23:01 -0600

    Fixing dates

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -23,14 +23,23 @@
         </div>
       </div>
 
-      <div class="post-date text-faded">
-        {{ post.publishedAt }}
+      <div
+        class="post-date text-faded"
+        :title="humanFriendlyDate(post.publishedAt)"
+      >
+        {{ diffForHumans(post.publishedAt) }}
       </div>
     </div>
   </div>
 </template>
 <script>
 import sourceData from "@/data.json";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+dayjs.extend(relativeTime);
+dayjs.extend(localizedFormat);
+
 export default {
   props: {
     posts: {
@@ -46,6 +55,12 @@ export default {
   methods: {
     userById(userId) {
       return this.users.find((p) => p.id === userId);
+    },
+    diffForHumans(timestamp) {
+      return dayjs.unix(timestamp).fromNow();
+    },
+    humanFriendlyDate(timestamp) {
+      return dayjs.unix(timestamp).format("llll");
     },
   },
 };
