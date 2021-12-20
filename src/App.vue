245d2340bdb15ac1074848468226eb0e8efbce21
yevgeller <yevgeller@gmail.com>
2021-12-19 22:56:42 -0600

    Created a NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <the-navbar />
   <div class="container"><router-view /></div>
 </template>
 
 <script>
+import TheNavbar from "@/components/TheNavbar";
 export default {
   name: "App",
+  components: {
+    TheNavbar,
+  },
 };
 </script>
 
