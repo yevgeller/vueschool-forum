2ec83ecc6b6aa8954ebc491a7535bd4b33dc70c0
yevgeller <yevgeller@gmail.com>
2022-01-09 15:31:43 -0600

    Using Class Bindings for User Drop Down Menu

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -15,7 +15,7 @@
     <nav class="navbar">
       <ul>
         <li v-if="authUser" class="navbar-user">
-          <router-link :to="{ name: 'Profile' }">
+          <a @click.prevent="userDropdownOpen = !userDropdownOpen">
             <img
               class="avatar-small"
               :src="authUser.avatar"
@@ -29,22 +29,24 @@
                 alt=""
               />
             </span>
-          </router-link>
+          </a>
 
           <!-- dropdown menu -->
           <!-- add class "active-drop" to show the dropdown -->
-          <div id="user-dropdown">
+          <div id="user-dropdown" :class="{ 'active-drop': userDropdownOpen }">
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
               <li class="dropdown-menu-item">
-                <a href="profile.html">View profile</a>
+                <router-link :to="{ name: 'Profile' }"
+                  >View profile</router-link
+                >
               </li>
-              <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+
+              <li class="dropdown-menu-item">
+                <a @click.prevent="$store.dispatch('signOut')">Sign Out</a>
+              </li>
             </ul>
           </div>
-        </li>
-        <li v-if="authUser" class="navbar-item">
-          <a @click.prevent="$store.dispatch('signOut')">Sign Out</a>
         </li>
         <li v-if="!authUser" class="navbar-item">
           <router-link :to="{ name: 'SignIn' }">Sign In</router-link>
@@ -60,6 +62,9 @@
 <script>
 import { mapGetters } from "vuex";
 export default {
+  data() {
+    return { userDropdownOpen: false };
+  },
   computed: {
     ...mapGetters(["authUser"]),
   },
