4dd9f32b2228dffe7c47bbcfb88dc540489387a4
yevgeller <yevgeller@gmail.com>
2022-01-18 21:07:10 -0600

    Fixing bug in PageScrollDirective

--- src/plugins/PageScrollDirective.js
+++ src/plugins/PageScrollDirective.js
@@ -1,11 +1,11 @@
 const PageScrollDirective = {
   mounted(el, binding) {
     el.__PageScroll__ = binding.value;
-    document.body.addEventListener("scroll", el.__PageScroll__);
+    document.addEventListener("scroll", el.__PageScroll__);
   },
 
   unmounted(el) {
-    document.body.removeEventListener("scroll", el.__PageScroll__);
+    document.removeEventListener("scroll", el.__PageScroll__);
   },
 };
 
