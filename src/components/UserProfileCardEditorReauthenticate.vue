ec276a6f2f937f1b257fdc98d23b10f57fb0ce5f
yevgeller <yevgeller@gmail.com>
2022-02-07 22:04:23 -0600

    Adding component to reauthenticate the user

---
+++ src/components/UserProfileCardEditorReauthenticate.vue
@@ -0,0 +1,24 @@
+<template>
+  <VeeForm>
+    <AppFormField
+      name="reauth-email"
+      label="Email"
+      v-model="email"
+      rules="email"
+    />
+    <AppFormField
+      name="reauth-password"
+      label="Password"
+      v-model="password"
+      type="password"
+    />
+    <button class="btn btn-green btn-small">Login</button>
+  </VeeForm>
+</template>
+<script>
+export default {
+  data() {
+    return { email: "", password: "" };
+  },
+};
+</script>
