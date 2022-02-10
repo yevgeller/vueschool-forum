a0869165803d50a6ebb1907ed58ab94a030214f4
yevgeller <yevgeller@gmail.com>
2022-02-09 21:48:04 -0600

    Updating user e-mail address

--- src/components/UserProfileCardEditor.vue
+++ src/components/UserProfileCardEditor.vue
@@ -118,7 +118,7 @@ export default {
   },
   setup() {
     const { addNotification } = useNotifications();
-    return addNotification;
+    return { addNotification };
   },
   data() {
     return {
